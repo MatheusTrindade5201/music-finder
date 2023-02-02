e56a2519592e4fdd46a1dda7db1b875f61388598
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-02-02 16:32:20 -0300

    feat: creating form to mobile devices

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import Form from '@/components/Form'
+import style from '@/styles/Login.module.css'
+
+const Login = () => {
+    return (
+        <main className={style.login__page}>
+            <Form />
+        </main>
+    )
+}
+
+export default Login
