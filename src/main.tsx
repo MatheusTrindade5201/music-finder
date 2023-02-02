fb1da556a3d88c7a1281aab5185a8f7f9499b2e9
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-02-02 08:53:24 -0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppRoutes from './routes'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <AppRoutes />
+  </React.StrictMode>,
+)
---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes } from "react-router-dom"
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+export default AppRoutes
