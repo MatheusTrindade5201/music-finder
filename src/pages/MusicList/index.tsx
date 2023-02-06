491e23792cfc03c892a010544717d27f8a92805f
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-02-06 17:03:18 -0300

    feat: creating the container to display the cards

---
+++ src/pages/MusicList/index.tsx
@@ -0,0 +1,15 @@
+import Card from '@/components/Card'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import style from '@/styles/MusicList.module.css'
+
+const MusicList = () => {
+    return (
+        <main className={style.musicList__page}>
+            <div className={style.musicList__container}>
+            </div>
+        </main>
+    )
+}
+
+export default MusicList
