67ce655357806192e551c8875ae5180d6bc2c06c
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-02-06 17:36:50 -0300

    feat: creating a loading animation to display while loading the music list

--- src/pages/MusicList/index.tsx
+++ src/pages/MusicList/index.tsx
@@ -1,6 +1,22 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 import style from '@/styles/MusicList.module.css'
+import { useState } from 'react'
 
 const MusicList = () => {
+
+    const [ isLoading, setIsLoading ] = useState<boolean>(true);
+    const [ list, setList ] = useState<Array<object>>([]);
+
+    if(isLoading){
+        return (
+            <main className={style.musicList__page}>
+                <span className={style.loading} />
+            </main>
+        )
+    }
+
+
     return (
         <main className={style.musicList__page}>
             <div className={style.musicList__container}>
