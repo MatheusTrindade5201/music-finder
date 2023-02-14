5d1ff99eae61b895dc642994c0334f427168c810
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-02-14 16:13:09 -0300

    feat: creating the discover music page - layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { DicoverMusicPage } from "./pages/DiscoverMusic"
 import Login from "./pages/Login"
 import MusicList from "./pages/MusicList"
 import StandardPage from "./pages/StandardPage"
@@ -9,7 +10,8 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/app" element={<StandardPage />}>
-          <Route index element={<MusicList />}/>
+          <Route path="/app/music-list" element={<MusicList />}/>
+          <Route path="/app/discover" element={<DicoverMusicPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
