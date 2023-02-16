cb1d5a36a12829e76198bee23b9608af6f607bd8
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-02-16 16:15:41 -0300

    feat: calling the method to the page

--- src/pages/DiscoverMusic/index.tsx
+++ src/pages/DiscoverMusic/index.tsx
@@ -1,14 +1,26 @@
+import { discoverMusic } from "@/service/api"
 import style from "@/styles/DiscoverMusic.module.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export const DicoverMusicPage = () => {
 
-    const [ active, setActive ] = useState<boolean>()
+    const navigate = useNavigate()
+
+    const [ recording, setRecording ] = useState<boolean>(false)
+    let isRecording = recording
+    const search = () => {
+        setRecording(!recording)
+        isRecording = !isRecording
+        if(isRecording){
+            discoverMusic({navigate})
+        }
+    }
 
     return (
         <main className={style.discoverMusic__page}>
-            <button onClick={() => setActive(!active)} className={style.discoverMusic__button}>
-                <span className={`${style.red__button} ${active ? style.active : ''}`}></span>
+            <button onClick={search} className={style.discoverMusic__button}>
+                <span className={`${style.red__button} ${recording ? style.active : ''}`}></span>
             </button>
         </main>
     )
