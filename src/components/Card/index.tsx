2d959ebeb2eb89540209cfeffc14567bb677aec0
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-02-17 16:51:52 -0300

    feat: making the CArd component reusable

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -5,12 +5,13 @@ interface Props {
     image: string,
     title: string,
     author: string,
-    id: string
+    id: string,
+    smallCard?: boolean
 }
 
-const Card = ({image, title, author, id} : Props) => {
+const Card = ({image, title, author, id, smallCard} : Props) => {
     return (
-        <Link className={style.card} to={`/app/${id}`}>
+        <Link className={`${style.card} ${smallCard ? style.smallCard : ''}`} to={`/app/${id}`}>
                 <img className={style.card__image} src={image} />
                 <div className={style.card__info}>
                     <h2 className={style.card__title}>{title}</h2>
