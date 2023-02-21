22268578a4c73a9cef76e23cdaaca2a2ff4af6f0
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-02-21 16:08:02 -0300

    fix: fixing missing classes in the component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,7 +2,7 @@ import style from '@/styles/Loading.module.css'
 
 const Loading = () => {
     return (
-        <main className={style.musicList__page}>
+        <main className={style.loading__page}>
                 <span className={style.loading} />
         </main>
     )
