cf133abb0391a4465c389d5d1e8d5e704091be06
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-02-21 16:02:05 -0300

    feat: creating a component to the loading element

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import style from '@/styles/Loading.module.css'
+
+const Loading = () => {
+    return (
+        <main className={style.musicList__page}>
+                <span className={style.loading} />
+        </main>
+    )
+}
+export default Loading
